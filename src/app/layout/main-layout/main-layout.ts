e8a33fb0b07516e0d3a5d8b5bc08f7cca5fe0979
Afia Farjana Mifa <afia@squarehealth.com.bd>
2025-10-21 23:45:31 +0600

    basic html layout

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '../header/header';
 import { Menu } from "../menu/menu";
+import { MainContent } from '../main-content/main-content';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [Header, Menu],
+  imports: [Header, Menu, MainContent],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss'
 })
